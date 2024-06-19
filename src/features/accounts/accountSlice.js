fb1dd82d7c0583822ecd9584fd2ab81aae9da98b
Ravi yadav <ravyyadav97@gmail.com>
2024-06-20 02:34:07 +0530

    fixed the imports

---
+++ src/features/accounts/accountSlice.js
@@ -0,0 +1,54 @@
+const initialStateAccount = {
+    balance: 0,
+    loan: 0,
+    loanPurpose: "",
+  };
+
+
+export default function accountReducer(state = initialStateAccount, action) {
+    //not allowed to modified state and side effect hello
+    //place as much as logic u can
+    switch (action.type) {
+      case "account/deposit":
+        return {
+          ...state,
+          balance: state.balance + action.payload,
+        };
+      case "account/withdraw":
+        return {
+          ...state,
+          balance: state.balance - action.payload,
+        };
+      case "account/requestLoan":
+        if (state.loan > 0) return state;
+        return {
+          ...state,
+          loan: action.payload.amount,
+          loanPurpose: action.payload.purpose,
+          balance: state.balance + action.payload.amount,
+        };
+      case "account/payLoan":
+        return {
+          ...state,
+          loan: 0,
+          loanPurpose: "",
+          balance: state.balance - state.loan,
+        };
+      default:
+        return state;
+    }
+  }
+
+  //action-creators
+ export function deposit(amount) {
+    return { type: "account/deposit", payload: amount };
+  }
+ export function withdraw(amount) {
+    return { type: "account/withdraw", payload: amount };
+  }
+ export function requestLoan(purpose, amount) {
+    return { type: "account/requestLoan", payload: { purpose, amount } };
+  }
+ export function payLoan() {
+    return { type: "account/payLoan" };
+  }
