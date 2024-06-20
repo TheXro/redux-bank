aaac7bcc9a718e53fcb90343e3b9259c3e98e54d
Ravi yadav <ravyyadav97@gmail.com>
2024-06-20 19:57:02 +0530

    added functionality

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deposit, withdraw, requestLoan } from "./accountSlice";
 
 function AccountOperations() {
   const [depositAmount, setDepositAmount] = useState("");
@@ -7,11 +9,29 @@ function AccountOperations() {
   const [loanPurpose, setLoanPurpose] = useState("");
   const [currency, setCurrency] = useState("USD");
 
-  function handleDeposit() {}
+  const dispatch = useDispatch();
 
-  function handleWithdrawal() {}
+  const {loan: currentLoan, loanPurpose: currentLoanPurpose, balance} = useSelector((store) => store.account);
+  
 
-  function handleRequestLoan() {}
+  function handleDeposit() {
+    if(!depositAmount) return;  
+    dispatch(deposit(depositAmount));
+    setDepositAmount("");
+  }
+
+  function handleWithdrawal() {
+    if(!withdrawalAmount) return;
+    dispatch(withdraw(withdrawalAmount));
+    setWithdrawalAmount("");
+  }
+
+  function handleRequestLoan() {
+    if(!loanAmount || !loanPurpose) return;
+    dispatch(requestLoan(loanPurpose, loanAmount));
+    setLoanAmount("");
+    setLoanPurpose("");
+  }
 
   function handlePayLoan() {}
 
@@ -65,11 +85,11 @@ function AccountOperations() {
           />
           <button onClick={handleRequestLoan}>Request loan</button>
         </div>
-
-        <div>
-          <span>Pay back $X</span>
-          <button onClick={handlePayLoan}>Pay loan</button>
-        </div>
+    
+      { currentLoan > 0 && (<div>
+        <span>Pay back ${currentLoan}({currentLoanPurpose})</span>
+        <button onClick={handlePayLoan}>Pay loan</button>
+      </div>)}
       </div>
     </div>
   );
--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,4 +1,6 @@
 import { useSelector } from "react-redux";
+import { createCustomer } from "./customerSlice";
+
 
 function Customer() {
   const customer = useSelector((state) => state.customer.fullName);
