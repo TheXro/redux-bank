afbe932689427b0ddd1a86864934b29f33b5426e
Ravi yadav <ravyyadav97@gmail.com>
2024-06-24 02:14:48 +0530

    fixed import

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
-import {configureStore} from "./store";
+import {configureStore} from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
-configureStore({
+const store = configureStore({
     reducer:{
         account: accountReducer,
         customer: customerReducer,
