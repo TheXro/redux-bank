05bf023a43c65ff3e398fc745fddadd103b0ace1
Ravi yadav <ravyyadav97@gmail.com>
2024-06-24 02:11:29 +0530

    added redux toolkit

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -56,7 +56,7 @@ function AccountOperations() {
             <option value="GBP">British Pound</option>
           </select>
 
-          <button onClick={handleDeposit} disabled={isLoading} >{isLoading ? "Converting..." : Deposit {depositAmount}}</button>
+          <button onClick={handleDeposit} disabled={isLoading} >{isLoading ? "Converting..." : `Deposit {${depositAmount}}`}</button>
         </div>
 
         <div>
--- src/store.js
+++ src/store.js
@@ -1,12 +1,10 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import {thunk} from "redux-thunk";
-import accountReducer from "./features/accounts/accountSlice";
-import customerReducer from "./features/customers/customerSlice";
+import {configureStore} from "./store";
 
 const rootReducer = combineReducers({
     account: accountReducer,
     customer: customerReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
-export default store;
+configureStore({})
+
+    export default store;
